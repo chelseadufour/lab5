528a12f077f609f25d4acd2ed74f6928553274b7
Chelsea DuFour <chelseadufour@gmail.com>
2016-07-05 15:43:26 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var temp;
+temp = prompt("Enter temperature for conversion");
+
+
+console.log(temp + " Temp in Fahrenheit");
+temp = ((temp-32)*5/9);
+document.write(temp);
+
+// console.log(temp + " Temp in Celsius");
+// temp = ((temp*9)/5+32);
+// document.write(temp);
+
+
+// var temp;
+// temp = prompt("Enter temp");
+
+
+// if (temp > 90) {
+// 	console.log("Wear shorts");
+// } else if (temp <= 89 && temp > 70) {
+// 	console.log("Still wear shorts");
+// } else if (temp <= 69 && temp > 55) {
+// 	console.log("nice jacket");
+// } else if (temp <= 54 && temp > 40) {
+// 	console.log("warm jacket");
+// } else {
+// 	console.log("winter");
+// }
+
+// for(var num = 1; num <= 100; num++) {
+// 	if (num%3 === 0 && num%5 === 0) {
+// 		console.log("fizzbuzz");
+// 	} else if (num%3 === 0) {
+// 		console.log("fizz");
+// 	} else if (num%5=== 0) {
+// 		console.log("buzz");
+// 	} else {
+// 		console.log(num);
+// 	}
+// }
+
